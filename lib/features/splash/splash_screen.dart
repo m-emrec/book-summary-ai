7bccd1de9ff74351b8f86df18ec95d51fe237a14
memrec45 <65484287+memrec45@users.noreply.github.com>
2024-12-01 19:10:44 +0300

    Initial commit for new project

---
+++ lib/features/splash/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:base_ap/core/constants/generated/assets.gen.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
+
+///
+class SplashScreen extends StatelessWidget {
+  /// A stateless widget that represents the splash screen of the application.
+  ///
+  /// This screen is typically displayed when the app is first launched,
+  /// and can be used to show a logo, loading indicator, or other introductory
+  /// content.
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    unawaited(
+      Future.delayed(
+        const Duration(seconds: 3),
+        () {
+          if (context.mounted) {
+            context.go("/");
+          }
+        },
+      ),
+    );
+    return Scaffold(
+      body: Center(
+        child: Lottie.asset(Assets.animations.aniSplash, repeat: false),
+      ),
+    );
+  }
+}
